3cfbb0db843a75ccb99de61dd54226c58a82ecd3
Tim Lochmüller <Spyker@tims-mbp.speedport_w_724v_typ_a_05011603_00_003>
2015-07-30 16:07:40 +0200

    Set also the time of the event, if it not a fullday event

--- Classes/Service/NewsOverwrite.php
+++ Classes/Service/NewsOverwrite.php
@@ -20,15 +20,22 @@ use TYPO3\CMS\Extbase\Reflection\ObjectAccess;
 class NewsOverwrite implements SingletonInterface {
 
 	/**
+	 * Day seconds
+	 *
+	 * @var int
+	 */
+	const DAY = 86400;
+
+	/**
 	 * @param News  $news
 	 * @param array $index
 	 */
 	public function overWriteNewsPropertiesByIndexArray(News $news, array $index) {
-		ObjectAccess::setProperty($news, 'datetime', new \DateTime('@' . $index['start_date']));
+		ObjectAccess::setProperty($news, 'datetime', new \DateTime('@' . $this->getCombinedTime($index['start_date'], $index['start_time'])));
 		ObjectAccess::setProperty($news, 'sorting', $index, TRUE);
 		if (ExtensionManagementUtility::isLoaded('eventnews')) {
 			ObjectAccess::setProperty($news, 'isEvent', TRUE, TRUE);
-			ObjectAccess::setProperty($news, 'eventEnd', new \DateTime('@' . $index['end_date']), TRUE);
+			ObjectAccess::setProperty($news, 'eventEnd', new \DateTime('@' . $this->getCombinedTime($index['end_date'], $index['end_time'])), TRUE);
 			ObjectAccess::setProperty($news, 'fullDay', (bool)$index['all_day'], TRUE);
 		}
 	}
@@ -44,8 +51,33 @@ class NewsOverwrite implements SingletonInterface {
 				->getTimestamp(),
 			'end_date'   => $index->getEndDate()
 				->getTimestamp(),
+			'start_time' => $index->isAllDay() ? 0 : $index->getStartTime(),
+			'end_time'   => $index->isAllDay() ? 0 : $index->getEndTime(),
 			'all_day'    => $index->isAllDay(),
 		);
 		$this->overWriteNewsPropertiesByIndexArray($news, $array);
+	}
+
+	/**
+	 * Get combined time
+	 *
+	 * - Remove the time of the date information
+	 * - Remove the date if the time information
+	 * - combine both values
+	 *
+	 * @param int $dateTimestamp
+	 * @param int $timeTimestamp
+	 *
+	 * @return int
+	 */
+	protected function getCombinedTime($dateTimestamp, $timeTimestamp) {
+		$dateTimestamp = floor($dateTimestamp / self::DAY) * self::DAY;
+		if ($timeTimestamp <= 0) {
+			return $dateTimestamp;
+		}
+		if ($timeTimestamp < self::DAY) {
+			return $dateTimestamp + $timeTimestamp;
+		}
+		return $dateTimestamp + ($timeTimestamp % self::DAY);
 	}
 }
