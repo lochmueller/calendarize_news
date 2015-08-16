ffe156184f53af6c6845c612fcc82f18394b609e
Tim Lochmueller <tim@fruit-lab.de>
2015-08-16 21:06:17 +0200

    Prepare release 0.3.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,19 +9,19 @@
 
 
 $EM_CONF['calendarize_news'] = array(
-	'title'            => 'Calendarize for News',
-	'description'      => 'Add Event options to the news extension',
-	'category'         => 'misc',
-	'version'          => '0.2.0',
-	'state'            => 'beta',
-	'clearcacheonload' => 1,
-	'author'           => 'Tim Lochmüller',
-	'author_email'     => 'tim@fruit-lab.de',
-	'constraints'      => array(
-		'depends' => array(
-			'typo3'       => '6.2.0-7.99.99',
-			'calendarize' => '1.4.1-0.0.0',
-			'news'        => '3.2.0-0.0.0',
-		),
-	),
+    'title'            => 'Calendarize for News',
+    'description'      => 'Add Event options to the news extension',
+    'category'         => 'misc',
+    'version'          => '0.3.0',
+    'state'            => 'beta',
+    'clearcacheonload' => 1,
+    'author'           => 'Tim Lochmüller',
+    'author_email'     => 'tim@fruit-lab.de',
+    'constraints'      => array(
+        'depends' => array(
+            'typo3'       => '6.2.0-7.99.99',
+            'calendarize' => '1.4.1-0.0.0',
+            'news'        => '3.2.0-0.0.0',
+        ),
+    ),
 );
