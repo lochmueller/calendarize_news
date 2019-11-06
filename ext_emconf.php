288c3f98dd07bf864604d6a635e20acf8480ab3a
Tim Lochmüller <tim@fruit-lab.de>
2019-11-06 23:22:25 +0100

    New versions of extensions

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,21 +9,21 @@
 
 
 $EM_CONF[$_EXTKEY] = [
-    'title'            => 'Calendarize for News',
-    'description'      => 'Add Event options to the news extension.',
-    'category'         => 'fe',
-    'version'          => '4.1.0',
-    'state'            => 'stable',
+    'title' => 'Calendarize for News',
+    'description' => 'Add Event options to the news extension.',
+    'category' => 'fe',
+    'version' => '4.1.0',
+    'state' => 'stable',
     'clearcacheonload' => 1,
-    'author'           => 'Tim Lochmüller',
-    'author_email'     => 'tim@fruit-lab.de',
-    'constraints'      => [
+    'author' => 'Tim Lochmüller',
+    'author_email' => 'tim@fruit-lab.de',
+    'constraints' => [
         'depends' => [
-            'php'         => '7.0.0-0.0.0',
-            'typo3'       => '8.7.0-9.5.99',
-            'calendarize' => '4.0.0-0.0.0',
-            'autoloader'  => '4.0.0-0.0.0',
-            'news'        => '7.0.0-7.99.99',
+            'typo3' => '8.7.13-9.5.99',
+            'php' => '7.0.0-0.0.0',
+            'calendarize' => '4.2.0-0.0.0',
+            'autoloader' => '6.0.0-0.0.0',
+            'news' => '7.2.0-7.99.99',
         ],
     ],
 ];
