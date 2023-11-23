09457ca53ac3e7e446d65de9f6b3c867a02a1bd3
Imko Schumacher <okmiim@live.de>
2023-11-23 18:29:53 +0100

    Remove autoloader

---
+++ Configuration/TCA/Overrides/sys_template.php
@@ -0,0 +1,9 @@
+<?php
+
+defined('TYPO3') or exit();
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+    'calendarize_news',
+    'Configuration/TypoScript',
+    'Calendarize News'
+);
