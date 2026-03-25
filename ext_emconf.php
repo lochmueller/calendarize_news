a6f6646c5c37c4db81e71394f8bead50a767e257
Sybille Peters <sypets@gmx.de>
2026-03-25 16:44:25 +0100

    !!![TASK] Drop v12 support

--- ext_emconf.php
+++ ext_emconf.php
@@ -17,8 +17,8 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'tim@fruit-lab.de',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.0-13.4.99',
-            'php' => '8.1.0-8.4.99',
+            'typo3' => '13.4.0-13.4.99',
+            'php' => '8.2.0-8.5.99',
             'calendarize' => '13.0.0-14.99.99',
             'news' => '12.0.0-12.99.99',
         ],
